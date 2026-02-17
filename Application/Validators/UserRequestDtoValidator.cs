51e63b647b84f527c846e1d5599ad2183fbaa6c9
Gabriela Miranda <mirandatech.5207418@gmail.com>
2026-02-17 17:48:07 -0300

    Mover pasta Tests para novo local Autora: Gabriela Miranda

--- Application/Validators/UserRequestDtoValidator.cs
+++ Application/Validators/UserRequestDtoValidator.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HR_Management_System
+namespace HR_Management_System.Application.Validators
 {
-    public class SystemUserServiceTests {
+    public class UserRequestDtoValidator
     {
         
     }
---
+++ Domain/Models/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HR_Management_System.Models;
+
+public partial class Request
+{
+    public int Id { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? Type { get; set; }
+
+    public int EmployeeId { get; set; }
+
+    public virtual Employee Employee { get; set; } = null!;
+}
