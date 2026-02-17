73856d65d6b1abfe3445fb363797eb942d6f1334
Gabriela Miranda <mirandatech.5207418@gmail.com>
2026-02-17 16:56:24 -0300

    feat: add unit test for SystemUserService
    - Criado teste unitário para o método GetAllAsync do SystemUserService
    - Utilizado xUnit como framework de testes e Moq para mock do repositório
    - Teste valida retorno correto da lista de usuários e a chamada ao repositório
    Autora: Gabriela Miranda

---
+++ Tests/SystemUserServiceTests .cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Management_System
+{
+    public class SystemUserServiceTests {
+    {
+        
+    }
+}
