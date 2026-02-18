b488fab110749bb4b1fb25bc2f7bf8e63631e79f
Patricia <patriciastarck@gmail.com>
2026-02-18 11:28:17 -0300

    feat: implementa repositório de funcionários

---
+++ HR-Management-System/Infrastructure/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using HR_Management_System.Models;
+
+namespace HR_Management_System.Infrastructure.Repositories;
+
+public interface IEmployeeRepository
+{
+    Task<bool> CpfExists(string cpf);
+    Task AddAsync(Employee employee);
+    Task SaveChangesAsync();
+    Task<Employee?> GetByIdAsync(int id);
+    Task<IEnumerable<Employee>> GetAllAsync();
+}
