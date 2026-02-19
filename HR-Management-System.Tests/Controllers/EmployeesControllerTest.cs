06264187b9edd2a4ceb171042ddf7b15498bb332
Gabriela Miranda <mirandatech.5207418@gmail.com>
2026-02-19 00:19:14 -0300

    UPDATE User e Employee

--- HR-Management-System.Tests/Controllers/EmployeesControllerTest.cs
+++ HR-Management-System.Tests/Controllers/EmployeesControllerTest.cs
@@ -1,4 +1,7 @@
-﻿using FluentValidation;
+﻿
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
@@ -8,83 +11,94 @@ using HR_Management_System.Application.Dtos;
 
 namespace HR_Management_System.Tests.Controllers
 {
-  [TestFixture]
-  public class EmployeesControllerTest
-  {
-    private Mock<IEmployeeService> _serviceMock;
-    private Mock<IValidator<EmployeeRequestDto>> _validatorMock;
-    private EmployeesController _controller;
-
-    [SetUp]
-    public void SetUp()
+    [TestFixture]
+    public class EmployeesControllerTest
     {
-      _serviceMock = new Mock<IEmployeeService>();
-      _validatorMock = new Mock<IValidator<EmployeeRequestDto>>();
-      _controller = new EmployeesController(_serviceMock.Object, _validatorMock.Object);
-    }
+        private Mock<IEmployeeService> _serviceMock;
+        private Mock<IValidator<EmployeeRequestDto>> _validatorMock;
+        private EmployeesController _controller;
 
-    [Test]
-    public async Task Create_ShouldReturnCreatedAtAction_WithValidData()
-    {
-      // ARRANGE
-      var request = new EmployeeRequestDto { Name = "Tobias", Cpf = "12345678901" };
-      var response = new EmployeeResponseDto { Id = 1, Name = "Tobias", Cpf = "12345678901" };
+        [SetUp]
+        public void SetUp()
+        {
+            _serviceMock   = new Mock<IEmployeeService>();
+            _validatorMock = new Mock<IValidator<EmployeeRequestDto>>();
+            _controller    = new EmployeesController(_serviceMock.Object, _validatorMock.Object);
 
-      _validatorMock.Setup(v => v.ValidateAsync(request, default))
-          .ReturnsAsync(new FluentValidation.Results.ValidationResult());
-      _serviceMock.Setup(s => s.CreateAsync(request)).ReturnsAsync(response);
-
-      // ACT
-      var result = await _controller.Create(request);
-
-      // ASSERT
-      var createdResult = result as CreatedAtActionResult;
-      Assert.That(createdResult, Is.Not.Null);
-      Assert.That(createdResult.ActionName, Is.EqualTo("GetById"));
-      Assert.That(((EmployeeResponseDto)createdResult.Value).Name, Is.EqualTo("Tobias"));
-    }
-
-    [Test]
-    public async Task Create_ShouldReturnValidationProblem_WhenDataIsInvalid()
-    {
-      // ARRANGE
-      var request = new EmployeeRequestDto { Name = "", Cpf = "invalid_cpf" };
-      var validationFailures = new List<FluentValidation.Results.ValidationFailure>
+            _controller.ControllerContext = new ControllerContext
             {
-                new FluentValidation.Results.ValidationFailure("Name", "Name is required."),
-                new FluentValidation.Results.ValidationFailure("Cpf", "Cpf is invalid.")
+                HttpContext = new DefaultHttpContext()
+            };
+        }
+
+        [Test]
+        public async Task Create_ShouldReturnCreatedAtAction_WithValidData()
+        {
+            // ARRANGE
+            var request  = new EmployeeRequestDto { Name = "Tobias", Cpf = "12345678901" };
+            var response = new EmployeeResponseDto { Id = 1, Name = "Tobias", Cpf = "12345678901" };
+
+            _validatorMock.Setup(v => v.ValidateAsync(request, default))
+                .ReturnsAsync(new ValidationResult());
+
+            _serviceMock.Setup(s => s.CreateAsync(request))
+                .ReturnsAsync(response);
+
+            // ACT
+            var result = await _controller.Create(request);
+
+            // ASSERT
+            var createdResult = result as CreatedAtActionResult;
+            Assert.That(createdResult, Is.Not.Null);
+            Assert.That(createdResult.ActionName, Is.EqualTo("GetById"));
+            Assert.That(((EmployeeResponseDto)createdResult.Value).Name, Is.EqualTo("Tobias"));
+        }
+
+        [Test]
+        public async Task Create_ShouldReturnValidationProblem_WhenDataIsInvalid()
+        {
+            // ARRANGE
+            var request = new EmployeeRequestDto { Name = "", Cpf = "invalid_cpf" };
+
+            var validationFailures = new List<ValidationFailure>
+            {
+                new ValidationFailure("Name", "Name is required."),
+                new ValidationFailure("Cpf", "Cpf is invalid.")
             };
 
-      _validatorMock.Setup(v => v.ValidateAsync(request, default))
-          .ReturnsAsync(new FluentValidation.Results.ValidationResult(validationFailures));
+            _validatorMock.Setup(v => v.ValidateAsync(request, default))
+                .ReturnsAsync(new ValidationResult(validationFailures));
 
-      // ACT
-      var result = await _controller.Create(request);
+            // ACT
+            var result = await _controller.Create(request);
 
-      // ASSERT
-      var badRequestResult = result as ObjectResult;
-      Assert.That(badRequestResult, Is.Not.Null);
-      Assert.That(badRequestResult.StatusCode, Is.EqualTo(400));
-      Assert.That(badRequestResult.Value, Is.InstanceOf<ValidationProblemDetails>());
+            // ASSERT
+            Assert.That(result, Is.Not.InstanceOf<CreatedAtActionResult>());
+            Assert.That(result, Is.Not.InstanceOf<OkObjectResult>());
 
-      var validationProblem = badRequestResult.Value as ValidationProblemDetails;
-      Assert.That(validationProblem.Errors.ContainsKey("Name"));
-      Assert.That(validationProblem.Errors["Name"], Does.Contain("Name is required."));
-      Assert.That(validationProblem.Errors.ContainsKey("Cpf"));
-      Assert.That(validationProblem.Errors["Cpf"], Does.Contain("Cpf is invalid."));
+            var objectResult = result as ObjectResult;
+            Assert.That(objectResult, Is.Not.Null);
+            Assert.That(objectResult.Value, Is.InstanceOf<ValidationProblemDetails>());
+
+            var validationProblem = objectResult.Value as ValidationProblemDetails;
+            Assert.That(validationProblem!.Errors.ContainsKey("Name"));
+            Assert.That(validationProblem.Errors["Name"], Does.Contain("Name is required."));
+            Assert.That(validationProblem.Errors.ContainsKey("Cpf"));
+            Assert.That(validationProblem.Errors["Cpf"], Does.Contain("Cpf is invalid."));
+        }
+
+        [Test]
+        public async Task GetById_ShouldReturnNotFound_WhenEmployeeDoesNotExist()
+        {
+            // ARRANGE
+            _serviceMock.Setup(s => s.GetByIdAsync(999))
+                .ReturnsAsync((EmployeeResponseDto?)null);
+
+            // ACT
+            var result = await _controller.GetById(999);
+
+            // ASSERT
+            Assert.That(result, Is.InstanceOf<NotFoundResult>());
+        }
     }
-
-    [Test]
-    public async Task GetById_ShouldReturnNotFound_WhenEmployeeDoesNotExist()
-    {
-      // ARRANGE
-      _serviceMock.Setup(s => s.GetByIdAsync(999)).ReturnsAsync((EmployeeResponseDto?)null);
-
-      // ACT
-      var result = await _controller.GetById(999);
-
-      // ASSERT
-      Assert.That(result, Is.InstanceOf<NotFoundResult>());
-    }
-  }
 }
