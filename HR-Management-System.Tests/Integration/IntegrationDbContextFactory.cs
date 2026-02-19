63e42955d55cae87d296544a0ca68c084b5e0cf0
Patricia <patriciastarck@gmail.com>
2026-02-19 18:45:22 -0300

    feat: implement EF Core initial migration and integration test infrastructure

---
+++ HR-Management-System.Tests/Integration/IntegrationDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using HR_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management_System.Tests.Integration;
+
+public static class IntegrationDbContextFactory
+{
+    private const string ConnectionString =
+        "Host=localhost;Port=5432;Database=hr_test;Username=postgres;Password=postgres";
+
+    public static RhContext Create()
+    {
+        var options = new DbContextOptionsBuilder<RhContext>()
+            .UseNpgsql(ConnectionString)
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        var context = new RhContext(options);
+
+        context.Database.EnsureDeleted();
+        context.Database.Migrate();
+
+        return context;
+    }
+}
